673a1b81cd8d84dcb165e48ef7d3d2438aedff29
GitHub Action <action@github.com>
2024-02-13 11:58:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/glasov/gitflic/services/MyProjectService.kt
+++ src/main/kotlin/com/github/glasov/gitflic/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.glasov.gitflic.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.glasov.gitflic.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
